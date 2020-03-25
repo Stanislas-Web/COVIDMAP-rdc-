cf9cff3ac7e4cf626c406b640fdfd4c255368293
ANORAK-MATTFLY <47450059+ANORAK-MATTFLY@users.noreply.github.com>
2020-03-25 15:10:22 +0100

    lk

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const user_db = new Datastore('user_coords.db')
 // BASIC CONFIGURATIONS
 app.listen(3000, () => console.log(`Server started listening at 3000`));
 app.use(express.static('static_files'));
-app.use(express.json({
-        limit: '1mb'
-    }))
-    //////////////////////////////////////
-    // CLIENT - SERVER INTERACTION SIDE
-user_db.loadDatabase();
-app.post("/user_geo_coords", (request, response) => {
-    const response_data = request.body;
-    console.log(response_data);
-    user_db.insert(response_data)
+// app.use(express.json({
+//         limit: '1mb'
+//     }))
+//////////////////////////////////////
+// CLIENT - SERVER INTERACTION SIDE
+// user_db.loadDatabase();
+// app.post("/user_geo_coords", (request, response) => {
+//     const response_data = request.body;
+//     console.log(response_data);
+//     user_db.insert(response_data)
 
-})
+// })
