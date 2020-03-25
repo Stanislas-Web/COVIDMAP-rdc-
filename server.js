458bf8b20aafc8bcf466d97fa76a73300eced2da
ANORAK-MATTFLY <47450059+ANORAK-MATTFLY@users.noreply.github.com>
2020-03-25 15:17:40 +0100

    port settings

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const user_db = new Datastore('user_coords.db')
 
 ///////////////////////////////////////
 // BASIC CONFIGURATIONS
-app.listen(3000, () => console.log(`Server started listening at 3000`));
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server started listening at 3000`));
 app.use(express.static('static_files'));
 app.use(express.json({
         limit: '1mb'
     }))
     ////////////////////////////////////
-CLIENT - SERVER INTERACTION SIDE
+    //CLIENT - SERVER INTERACTION SIDE
 user_db.loadDatabase();
 app.post("/user_geo_coords", (request, response) => {
     const response_data = request.body;
